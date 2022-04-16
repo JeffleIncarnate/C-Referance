c841b711f8f757d92d48da6e0c96535046e7102d
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-04-16 18:36:31 +1200

    First commit

---
+++ Variables/Variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main() {
+  int characterAge = 15;
+  char characterName[] = "Dhruv Rayat";
+  bool isMale = true;
+  float gpa = 14.243;
+
+  printf("Character Name: %s \n", characterName);
+  printf("Chacter Age %d \n", characterAge);
+  printf("Character GPA %f ", gpa);
+
+  return 0;
+}
