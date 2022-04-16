af403b10cbda88d3537325b4eb4acab5de037af9
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-04-17 11:53:53 +1200

    added Calulator Code

---
+++ User Input/UserInput.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int age;
+  double gpa;
+  char grade;
+  char name[20];
+
+  age = printf("Enter your name: ");
+
+  // This is better, but can only store one number
+  scandf("%s", name)
+  // General, can store more than one word
+  fgets(name, 20, stdin);
+
+  printf("Your name is %s!", name);
+
+  return 0;
+}
