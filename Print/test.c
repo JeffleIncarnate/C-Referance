7bff28ed5a8342874682240e0c0a0e7cd027cd79
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-04-16 19:14:39 +1200

    Comments for it

--- Print/test.c
+++ Print/test.c
@@ -1,7 +1,10 @@
+// Libraries
 #include <stdio.h>
 #include <stdlib.h>
 
+// Main Funtction
 int main() {
+  // Printf
   printf("Hello world\n");
   return 0;
 }
--- Variables/Variables.c
+++ Variables/Variables.c
@@ -1,15 +1,31 @@
+// Libraries
 #include <stdio.h>
 #include <stdlib.h>
+
+// You need this to get a bool variable with C
 #include <stdbool.h>
 
+// Main Function
 int main(void) {
+  // Int
   int characterAge = 15;
+
+  // String = Array of chars "I KNOW STUPID"
   char characterName[] = "Dhruv Rayat";
+
+  // Bool: True or False
   bool isMale = true;
+
+  // Float: Floating point decimals
   float gpa = 14.243;
 
+  // Print Character name "%s" stands for string as a placeholder for the string var
   printf("Character Name: %s \n", characterName);
+
+  // Print Character Age "%d" stand for int as a placehodler for the int var
   printf("Chacter Age %d \n", characterAge);
+
+  // Print the Character GPA "%f" stands for int as a placeholder for the float var
   printf("Character GPA %f ", gpa);
 
   return 0;
