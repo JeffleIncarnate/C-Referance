99c1489e624dce802d6bbdb5a9de63195bbab547
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-04-17 14:50:33 +1200

    Added Arrays folder

---
+++ Arrays/Arrays.c
@@ -0,0 +1,19 @@
+// Libraries
+#include <stdio.h>
+
+// Main Function
+int main() {
+  // Array of ints "[10]"means the array is empty, but it has the potentional to hold 10 numbers
+  int luckyNumbers[10]; // Empty array
+  int nums[] = { 10, 11, 12, 13, 14 }; // Has 5 items
+
+  // Strings == Char arrays
+  char name[] = "Dhruv";
+
+  // Prints name array or string
+  printf("%s\n", name);
+  // Print Lucy numbers index of 1
+  printf("%d", nums[1]);
+
+  return 0;
+}
