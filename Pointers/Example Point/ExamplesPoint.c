04d9a03ddaec7a26d8a4160d97359c703ead4a24
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-05-05 12:11:30 +1200

    Added Pointers

---
+++ Pointers/Example Point/ExamplesPoint.c
@@ -0,0 +1,16 @@
+// Main Library
+#include <stdio.h>
+
+// Main Function
+int main() {
+    // Vars Declared
+    int var1 = 10;
+    char var2[10];
+
+    // Printing the memory address
+    printf("Adress of var1 varaible is: %x\n", var1);        
+    printf("Adress of var2 varaible is: %x\n", var2);    
+
+    // Retun
+    return 0;
+}
