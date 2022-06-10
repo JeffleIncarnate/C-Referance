2f24e003dbd4b84a5008fa8895a1107f380f979b
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-06-11 03:37:06 +1200

    Got user unput from var

---
+++ Tests/Unit Converter/converter.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char from[2];
+    char to[2];
+    int amount;
+    int converted
+
+    printf("Welcome to the best Unit Converter! (only length)");
+    printf("WE only use SI units, since we aren't fucking crazy.");
+    
+    // Converter from
+    printf("Please select a the unit you'd like to convert from: ");
+    printf("km (kilometres), m (metre), cm, (centimetre), mm (milimetre)");
+    printf(": ");
+    scanf("%s", from);
+
+
+
+
+    return 0;
+}
